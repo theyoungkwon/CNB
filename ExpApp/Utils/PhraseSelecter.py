574fd26bbfe5a77709c09ed78de6129b336f4633
theyoungkwon <youngkwon311@gmail.com>
2020-02-11 17:45:07 +0800

    MYOKEY added a select_word function to PhraseSelecter class

--- ExpApp/Utils/PhraseSelecter.py
+++ ExpApp/Utils/PhraseSelecter.py
@@ -12,13 +12,26 @@ class PhraseSelecter:
                 self.phase_list.append(line)
         print ("\nPhases are loaded and the number of phases is : ", len(self.phase_list))
 
-    def select_phase(self) -> "":
-        return random.choice(self.phase_list)
+    def select_phrase(self) -> "":
+        return random.choice(self.phase_list).strip()
+
+    def select_word(self) -> "":
+        while(1):
+            phrase = random.choice(self.phase_list)
+            word_list = phrase.strip().split()
+            for word in word_list:
+                if len(word) == 5:
+                    return word.lower()
 
 def main() -> None:
     a = PhraseSelecter()
+    print("TESTING:: PhraseSelecter.select_phrase()")
     for i in range(10):
-        print(a.select_phase())
+        print(a.select_phrase())
+
+    print("\nTESTING:: PhraseSelecter.select_word()")
+    for i in range(10):
+        print(a.select_word())
 
 if __name__ == "__main__":
     main()
